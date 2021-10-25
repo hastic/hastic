7c507f0f86a2b2af28a1ecf9f063248d739a6b13
Alexey Velikiy <cg@corpglory.com>
2021-10-26 01:44:16 +0300

    query metric

--- client/src/components/Graph.vue
+++ client/src/components/Graph.vue
@@ -5,6 +5,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { HasticPod } from "./hastic_pod";
+import { getMetrics } from '../services/metrics.service';
+
+import _ from "lodash";
 
 // TODO: fetch data from API
 // TODO: set height
@@ -12,28 +15,39 @@ export default defineComponent({
   name: 'Graph',
   props: {},
   mounted() {
-    const startTime = 1590590148;
-    const arrayLength = 20;
-    const data1: [number, number][] = Array.from({ length: arrayLength }, (el, idx) => [Math.floor(Math.random() * 40), startTime + idx * 10000]);
-    // const zoomIn = (ranges) => { const range = ranges[0]; options.axis.x.range = range; pod.updateData(undefined, options) }
-    // const zoomOut = (ranges) => { console.log('zoomout'); options.axis.x.range = undefined; pod.updateData(undefined, options) }
-    let options = {
-      renderLegend: false, 
-      usePanning: false, 
-      // axis: {
-      //    y: { invert: false, range: [0, 350] },
-      //    x: { format: 'time' }
-      // },
-      // eventsCallbacks: { zoomIn: zoomIn, zoomOut }
-    }
-    var pod = new HasticPod(
-      document.getElementById('chart'),
-      [
-        { target: 'test1', datapoints: data1, color: 'green' },
-      ],
-      options
-    );
-    pod.render();
+    const startTime = 1634172070;
+    const endTime = 1635110190;
+    const step = Math.round((endTime - startTime) / 5000);
+    
+    getMetrics(startTime, endTime, step).then((res) => {
+      // console.log(res);
+      let target = _.keys(res["data"]["data"])[0];
+      let values = res["data"]["data"][target].map(([a,b]) => [b,a]);
+
+      
+      
+      // const zoomIn = (ranges) => { const range = ranges[0]; options.axis.x.range = range; pod.updateData(undefined, options) }
+      // const zoomOut = (ranges) => { console.log('zoomout'); options.axis.x.range = undefined; pod.updateData(undefined, options) }
+      let options = {
+        renderLegend: false, 
+        usePanning: false, 
+        // axis: {
+        //    y: { invert: false, range: [0, 350] },
+        //    x: { format: 'time' }
+        // },
+        // eventsCallbacks: { zoomIn: zoomIn, zoomOut }
+      }
+      var pod = new HasticPod(
+        document.getElementById('chart'),
+        [
+          { target: target, datapoints: values, color: 'green' },
+        ],
+        options
+      );
+      pod.render();
+    })
+    
+    
   }
 });
 </script>
--- client/src/services/metrics.service.ts
+++ client/src/services/metrics.service.ts
@@ -1,9 +1,10 @@
 import { API_URL } from "@/config";
 import axios from 'axios';
 
-const METRICS_API_URL = API_URL + "metrics";
+const METRICS_API_URL = API_URL + "metric/";
 
-export default async function getMetrics(from: number, to: number, step: number) {
-  let uri = METRICS_API_URL + `?from=${from}`;
-  let res = axios.get(METRICS_API_URL);
+export async function getMetrics(from: number, to: number, step: number) {
+  const uri = METRICS_API_URL + `?from=${from}&to=${to}&step=${step}`;
+  const res = axios.get(uri);
+  return res;
 }
